3447d46e2b8e225bbb7724e8699949f57cd93c04
Sam Symons <sam@samsymons.com>
2022-01-25 12:19:14 -0800

    Add a basic script to copy private Xcode Cloud resources.

---
+++ ci_scripts/install_xcode_cloud_resources.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e # Fail if any command fails
+set -x # Debug log
+
+DIRECTORY_NAME=XcodeCloudResources
+
+git clone "$XCODE_CLOUD_RESOURCES_URL" $DIRECTORY_NAME
+
+ls -la $DIRECTORY_NAME/Fonts
+
+cp $DIRECTORY_NAME/Fonts/*.otf "fonts/licensed"
